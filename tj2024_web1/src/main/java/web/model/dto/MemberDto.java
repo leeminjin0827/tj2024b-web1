d67dec9c8fb67563cb88797ac4a6c3432afc9b6f
leeminjin0827 <lmj98202@gmail.com>
2025-01-27 10:23:23 +0900

    day08

---
+++ tj2024_web1/src/main/java/web/model/dto/MemberDto.java
@@ -0,0 +1,21 @@
+package web.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter @ToString
+
+public class MemberDto {
+	
+	private int mno;		// 회원번호
+	private String mid;		// 아이디 
+	private String mpwd;	// 비밀번호
+	private String mname;	// 이름
+	private String mphone;	// 연락처 
+	private String mdate;	// 가입일 
+	
+} // c end
